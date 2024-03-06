544f05782e4594cc25bb9fe74e603f6c52533cae
nadia <nadiajuckova@gmail.com>
2024-03-06 14:44:15 +0100

    Fix stars bug

--- scripts/script.js
+++ scripts/script.js
@@ -21,30 +21,31 @@ function switchMode() {
 	}
 }	
 
-
-let sky, center
+let sky, center;
 
 function dot(i) {
-   const size = Math.round(Math.random() + 1)
-   const root = document.createElement('span')
-   root.style.top = center.y + 'px'
-   root.style.left = center.x + 'px'
-   root.classList.add('star', `size-${size}`, `axis-${i}`)
-   return root
+   const size = Math.round(Math.random() + 1);
+   const root = document.createElement('span');
+   const x = Math.random() * sky.clientWidth;
+   const y = Math.random() * sky.clientHeight;
+   root.style.top = y + 'px';
+   root.style.left = x + 'px';
+   root.classList.add('star', `size-${size}`, `axis-${i}`);
+   return root;
 }
 
 function clear() {
-   sky.innerHTML = ''
+   sky.innerHTML = '';
 }
 
 function init() {
-   sky = document.querySelector('#sky')
+   sky = document.querySelector('#sky');
    center = {
       x: sky.clientWidth / 2,
       y: sky.clientHeight / 2,
-   }
-   clear()
-   for (let i = 0; i < 360; i++) sky.appendChild(dot(i))
+   };
+   clear();
+   for (let i = 0; i < 360; i++) sky.appendChild(dot(i));
 }
 
-window.onload = init
+window.onload = init;
