52d7707ca7d87bc7cc23965f9f1ea5e50f0080ff
Theo <theo.serralta@gmail.com>
2024-04-06 00:05:42 +0200

    Add questions and animations to quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,101 +2,106 @@ const myQuestions = [
   {
     type: 'boolean',
     difficulty: 'easy',
-    category: 'Science: Computers',
-    question: 'Ada Lovelace is often considered the first computer programmer.',
-    correct_answer: 'True',
-    incorrect_answers: ['False'],
+    category: 'Animals',
+    question: 'Tous les oiseaux savent voler.',
+    correct_answer: 'Faux',
+    incorrect_answers: ['Vrai'],
   },
   {
     type: 'multiple',
     difficulty: 'easy',
-    category: 'Entertainment: Music',
-    question:
-      'The band Muse released their first album, Showbiz, in what year?',
-    correct_answer: '1999',
-    incorrect_answers: ['1998', '2000', '2001'],
+    category: 'Animal',
+    question: 'Quel est cet oiseau ?',
+    image: '../styles/Oiseaux/Merle_noir.jpg',
+    correct_answer: 'Merle noir',
+    incorrect_answers: ['Pigeon ramier', 'Mésange charbonnière', 'Coucou gris'],
   },
   {
     type: 'multiple',
     difficulty: 'medium',
-    category: 'Geography',
-    question:
-      'All of the following countries have official claims to territory in Antarctica EXCEPT:',
-    correct_answer: 'United States',
-    incorrect_answers: ['Australia', 'Chile', 'Norway'],
-  },
-  {
-    type: 'multiple',
-    difficulty: 'medium',
-    category: 'Entertainment: Video Games',
-    question:
-      'Which of these games was NOT a Nintendo Switch launch title in the United States? ',
-    correct_answer: 'Voez',
-    incorrect_answers: ['Just Dance 2017', 'Snipperclips', 'Fast RMX'],
-  },
-  {
-    type: 'multiple',
-    difficulty: 'medium',
-    category: 'Science &amp; Nature',
-    question: 'Which of these is a type of stretch/deep tendon reflex?',
-    correct_answer: 'Ankle jerk reflex',
-    incorrect_answers: [
-      'Gag reflex',
-      'Pupillary light reflex',
-      'Scratch reflex',
-    ],
-  },
-  {
-    type: 'multiple',
-    difficulty: 'medium',
-    category: 'Entertainment: Video Games',
-    question: 'When was the Sega Genesis released in Japan?',
-    correct_answer: 'October 29, 1988',
-    incorrect_answers: [
-      'August 14, 1989',
-      'November 30, 1990',
-      'September 1, 1986',
-    ],
+    category: 'Animal',
+    question: 'Quel est cet oiseau ?',
+    image: '../styles/Oiseaux/pic_epeiche.jpg',
+    correct_answer: 'Pic épeiche',
+    incorrect_answers: ['Pic vert', 'Pic noir', 'Pic mar'],
   },
   {
     type: 'boolean',
     difficulty: 'medium',
-    category: 'Science &amp; Nature',
-    question:
-      'Scientists accidentally killed the once known world&#039;s oldest living creature, a mollusc, known to be aged as 507 years old.',
+    category: 'Animal',
+    question: "En France, la période de nidification des oiseaux s'étend de mi-Mars à mi-Août.",
+    correct_answer: 'Vrai',
+    incorrect_answers: ['Faux'],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'medium',
+    category: 'Animal',
+    question: 'Cet oiseau est-il le préféré de Nadya ou de Théo ? (Le Martin-pêcheur)',
+    image: '../styles/Oiseaux/Martin_pecheur.jpg',
+    correct_answer: 'Théo',
+    incorrect_answers: ['Nadya'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'hard',
+    category: 'Animal',
+    question: 'Quel est cet oiseau ?',
+    image: '../styles/Oiseaux/Sitelle_Torchepot.jpg',
+    correct_answer: 'Sitelle torchepot',
+    incorrect_answers: ["Verdier d'Europe", 'Mésange bleue', 'Pouillot véloce'],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'easy',
+    category: 'Animal',
+    question: "Il n'y a pas de corbeaux à Paris.",
+    image: '../styles/Oiseaux/Grand_Corbeau.jpg',
     correct_answer: 'True',
     incorrect_answers: ['False'],
   },
   {
     type: 'multiple',
-    difficulty: 'easy',
-    category: 'Entertainment: Film',
-    question: 'When was the movie &#039;Con Air&#039; released?',
-    correct_answer: '1997',
-    incorrect_answers: ['1985', '1999', '1990'],
+    difficulty: 'hard',
+    category: 'Animal',
+    question: "Quel oiseau n'existe pas ?",
+    correct_answer: 'Galinette cendrée',
+    incorrect_answers: ['Bouscarle de cetti', 'Bruant zizi', 'Chevalier cul-blanc'],
   },
   {
     type: 'multiple',
     difficulty: 'medium',
-    category: 'Art',
-    question: 'Who designed the Chupa Chups logo?',
-    correct_answer: 'Salvador Dali',
-    incorrect_answers: ['Pablo Picasso', 'Andy Warhol', 'Vincent van Gogh'],
+    category: 'Animal',
+    question: 'Quel est cet oiseau ?',
+    image: '../styles/Oiseaux/Buse_variable.jpg',
+    correct_answer: 'Buse variable',
+    incorrect_answers: ['Milan noir', 'Aigle royal', 'Faucon pelerin'],
   },
   {
     type: 'multiple',
     difficulty: 'medium',
-    category: 'Entertainment: Books',
-    question:
-      'What was the name of the Mysterious Island, in Jules Verne&#039;s &quot;The Mysterious Island&quot;?',
-    correct_answer: 'Lincoln Island',
-    incorrect_answers: [
-      'Vulcania Island',
-      'Prometheus Island',
-      'Neptune Island',
-    ],
+    category: 'Animal',
+    question: "Quel oiseau n'existe pas en France ?",
+    correct_answer: 'Héron goliath',
+    incorrect_answers: ["Ouette d'Egypte", 'Perruche à collier', 'Tourterelle turque'],
   },
 ];
+
+// Fonction pour mélanger un tableau
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Mélanger le tableau de questions
+shuffleArray(myQuestions);
+
+// Mélanger les réponses pour chaque question
+myQuestions.forEach(question => {
+  shuffleArray(question.incorrect_answers);
+});
 
 let questions = [];
 let stats = {
@@ -182,12 +187,25 @@ function initiateGame() {
 function displayQuestion(questionObject) {
   const quizQuestion = document.querySelector('#quiz-question');
   const quizOptions = document.querySelector('#quiz-options');
+  const quizImage = document.querySelector('#quiz-image');
+  quizQuestion.innerHTML = '';
+  quizOptions.innerHTML = '';
 
   quizQuestion.classList.add('zoomOut');
   quizOptions.classList.add('zoomOut');
 
   setTimeout(() => {
     quizQuestion.innerHTML = questionObject.question;
+
+    if (questionObject.image) {
+      const image = document.createElement('img');
+      image.src = questionObject.image;
+      image.className = 'fadeIn animated';
+      quizImage.innerHTML = '';
+      quizImage.appendChild(image);
+    } else {
+      quizImage.innerHTML = ''; // Clear if no image
+    }
 
     quizQuestion.classList.remove('zoomOut');
     quizOptions.classList.remove('zoomOut');
@@ -224,7 +242,7 @@ function displayQuestion(questionObject) {
         console.log('Invalid question object or missing answers property.');
       }
     }, 0.01);
-  }, 500);
+  }, 1000);
 }
 
 function nextQuestion() {
