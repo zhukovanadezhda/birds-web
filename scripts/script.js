4e0eee4de073399e1d59126c12c9a09c142f6bf7
nadia <nadiajuckova@gmail.com>
2024-04-05 16:43:07 +0200

    Fix day/night mode bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,40 @@
-/* switch between dark and light mode */
-
+// Function to switch between dark and light mode
 function switchMode() {
-	let moon = document.getElementById("moon");
-	let navLinks = document.querySelectorAll("nav ul li a");
+    let moon = document.getElementById("moon");
+    let navLinks = document.querySelectorAll("nav ul li a");
 
-	if (moon.className == "moon") {
-		moon.className = "sun";
-		document.body.style.backgroundColor = "#141D26";
-		document.body.style.color = "#FFFFFF";
-		// Update menu text color to white
-		navLinks.forEach(link => {
-			link.style.color = "#FFFFFF";
-		});
-	} else {
-		moon.className = "moon";
-		document.body.style.backgroundColor = "#FFFFFF";
-		document.body.style.color = "#000000";
-		// Update menu text color to black
-		navLinks.forEach(link => {
-			link.style.color = "#000000";
-		});
-	}
-}	
+    if (moon.className == "moon") {
+        moon.className = "sun";
+        document.body.style.backgroundImage = "url('../styles/images/back_night.jpg')";
+        document.body.style.color = "#FFFFFF";
+        // Update menu text color to white
+        navLinks.forEach(link => {
+            link.style.color = "#FFFFFF";
+        });
+        // Save mode selection to localStorage
+        localStorage.setItem('mode', 'dark');
+    } else {
+        moon.className = "moon";
+        document.body.style.backgroundImage = "url('../styles/images/back_day.jpg')";
+        document.body.style.color = "#000000";
+        // Update menu text color to black
+        navLinks.forEach(link => {
+            link.style.color = "#000000";
+        });
+        // Save mode selection to localStorage
+        localStorage.setItem('mode', 'light');
+    }
+}
+
+// Check if mode is stored in localStorage and apply it on page load
+document.addEventListener('DOMContentLoaded', function() {
+    let mode = localStorage.getItem('mode');
+    if (mode === 'dark') {
+        switchMode(); // Apply dark mode
+    }
+    // If mode is not stored or it's 'light', keep the default light mode
+});
+
 
 /* add stars to the background */
 
