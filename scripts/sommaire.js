c7925abdca1b13c29be4393ebc375c8b5068b17a
nadia <nadiajuckova@gmail.com>
2024-04-06 20:18:02 +0200

    Update summary page

---
+++ scripts/sommaire.js
@@ -0,0 +1,15 @@
+// Smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
